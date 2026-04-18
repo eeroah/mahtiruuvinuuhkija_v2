720da5b2ad2350f6c99af9ed81dfb53456619549
Eero Ahola <eeroah@crasman.fi>
2026-04-18 17:07:01 +0300

    Cleanup: Remove old code, correct MAC formatting, fix CSS background scroll

--- test-ruuvi.mjs
+++
@@ -1,8 +0,0 @@
-import ruuvi from 'node-ruuvitag';
-ruuvi.on('found', tag => {
-  tag.on('updated', data => {
-    console.log('Got data from tag:', tag.id);
-    console.log(data);
-    process.exit(0);
-  });
-});
